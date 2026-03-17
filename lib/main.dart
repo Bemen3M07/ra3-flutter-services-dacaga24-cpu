693b26dfba85a05009843432dabad46859a9636d
dacaga-hub <daniel.caravacagarcia@gmail.com>
2026-03-17 16:47:22 +0100

    commit - canviat main per mostrar també acudits

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'ex2_cars_ui/provider/cars_provider.dart';
 import 'ex2_cars_ui/view/cars_list_view.dart';
+import 'ex3_jokes/provider/joke_provider.dart';
+import 'ex3_jokes/view/joke_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,14 +17,53 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => CarsProvider()),
+        ChangeNotifierProvider(create: (_) => JokeProvider()),
       ],
       child: MaterialApp(
-        title: 'Cars API',
+        title: 'APIs i Serveis',
+        debugShowCheckedModeBanner: false,
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const CarsListView(),
+        home: const MainNavigator(),
+      ),
+    );
+  }
+}
+
+class MainNavigator extends StatefulWidget {
+  const MainNavigator({super.key});
+
+  @override
+  State<MainNavigator> createState() => _MainNavigatorState();
+}
+
+class _MainNavigatorState extends State<MainNavigator> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    CarsListView(),
+    JokeView(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.directions_car),
+            label: 'Cars',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.emoji_emotions),
+            label: 'Jokes',
+          ),
+        ],
       ),
     );
   }
