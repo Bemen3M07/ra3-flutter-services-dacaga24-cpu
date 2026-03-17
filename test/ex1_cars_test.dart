c8240da3c19d3a7b607e43f1d55723115f288c0c
dacaga-hub <daniel.caravacagarcia@gmail.com>
2026-03-17 12:21:30 +0100

    commit - creat el ex1_cars_test.dart i canviat el nom a yaml

---
+++ test/ex1_cars_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:ra3_flutter_services_dacaga24_cpu/ex1_cars/service/car_http_service.dart';
+
+void main() {
+  group('CarsApi', () {
+    test('get cars', () async {
+      final carHttpService = CarHttpService();
+      final cars = await carHttpService.getCars();
+
+      // Comprovem que hem rebut 10 cotxes
+      expect(cars.length, 10);
+
+      // Comprovem que el primer i l'últim tenen els ids esperats
+      expect(cars[0].id, 9582);
+      expect(cars[9].id, 9591);
+
+      // Comprovem que els camps no estan buits
+      expect(cars[0].make, isNotEmpty);
+      expect(cars[0].model, isNotEmpty);
+      expect(cars[0].type, isNotEmpty);
+    });
+  });
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_hello_world/main.dart';
+import 'package:ra3_flutter_services_dacaga24_cpu/main.dart';
 
 void main() {
   testWidgets('Hello World screen displays correctly', (WidgetTester tester) async {
